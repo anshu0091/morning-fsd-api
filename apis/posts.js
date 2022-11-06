c25f4cc6caa0552fd9b5409d3c710fe1c982bb98
anshu0091 <110275008+anshu0091@users.noreply.github.com>
2022-11-06 08:17:09 +0530

    Update posts.js

--- apis/posts.js
+++ apis/posts.js
@@ -1,127 +1,147 @@
-import { Router } from "express";
-import fs from "fs";
-import { ObjectId } from "mongodb";
-import {
-  getAllItemsFromCollection,
-  saveItemInCollection,
-  updateItemFromCollection,
-} from "../utilities/mongo-wrapper.js";
+
+
+
+import { Router } from 'express';
+import fs from 'fs'
+import { ObjectId } from 'mongodb';
+import { getAllItemsFromCollection, saveItemInCollection, updateItemFromCollection } from '../utilities/mongo-wrapper.js';
 
 const postRoutes = Router();
 
+
 // get all
-postRoutes.get("/posts", async (req, res) => {
-  let postsList = await getAllItemsFromCollection("posts", {});
-  return res.json(postsList);
-});
+postRoutes.get('/posts', async (req, res) => {
+    let postsList = await getAllItemsFromCollection("posts", {})
+    return res.json(postsList)
+})
+
 
 //get single
-postRoutes.get("/posts/:id", async (req, res) => {
-  let id = req.params.id;
+postRoutes.get('/posts/:id', async (req, res) => {
 
-  let postsList = await getAllItemsFromCollection("posts", {
-    _id: ObjectId(id),
-  });
+    let id = req.params.id;
 
-  return res.json(postsList[0]);
-});
+    let postsList = await getAllItemsFromCollection("posts", { _id: ObjectId(id) })
 
-//create
-postRoutes.post("/posts", async (req, res) => {
-  let obj = req.body;
+    return res.json(postsList[0])
+})
 
-  obj["likes"] = 0;
-  obj["likedUsers"] = [];
-  obj["comments"] = [];
 
-  await saveItemInCollection("posts", obj);
-  //   await saveItemInCollection("comments", obj);
-  return res.json({
-    result: true,
-  });
-});
 
-postRoutes.delete("/posts", (req, res) => {
-  //delete login
+//create 
+postRoutes.post('/posts', async (req, res) => {
 
-  return res.json({
-    result: true,
-  });
-});
 
-postRoutes.put("/posts", (req, res) => {
-  return res.json({
-    result: true,
-  });
-});
+    let obj = req.body;
 
-//likes
-postRoutes.patch("/posts/:id/like", async (req, res) => {
-  let id = req.params.id;
-  let username = req.body.username;
-  //get item from database
-  let posts = await getAllItemsFromCollection("posts", { _id: ObjectId(id) });
+    obj["likes"] = 0;
+    obj["likedUsers"] = []
+    obj["comments"] = []
 
-  //checking if found
-  if (posts && posts.length > 0) {
-    console.log("post preset");
 
-    let post = posts[0];
-    if (!post.likedUsers.some((x) => x == username)) {
-      //update likes on server
-      post.likedUsers.push(username);
-      post.likes = post.likedUsers.length;
+    await saveItemInCollection("posts", obj)
 
-      //update likes on database
-      await updateItemFromCollection("posts", { _id: ObjectId(id) }, post);
-      //done
-      return res.json({
-        result: true,
-        newLikes: post.likes,
-      });
-    } else {
-      return res.json({
-        result: true,
-        newLikes: post.likes,
-      });
-    }
-  }
-
-  return res.json({
-    result: false,
-  });
-});
-
-//likes
-postRoutes.post("/posts/:id/comments", async (req, res) => {
-  let id = req.params.id;
-  let commentObj = req.body;
-  //get item from database
-  post.comments.push(commentObj);
-  let postsList = await getAllItemsFromCollection("posts", {
-    _id: ObjectId(id),
-  });
-  // JSON.parse(fs.readFileSync('./data/posts.json'))
-  let index = postsList.findIndex((x) => x.id == id);
-
-  //checking if found
-  if (index > -1) {
-    let post = postsList[index];
-    post.comments.push(commentObj);
-
-    //save the file
-    await updateItemFromCollection("posts", { _id: ObjectId(id) }, post);
-    // fs.writeFileSync('./data/posts.json', JSON.stringify(postsList))
-    //done
     return res.json({
-      result: true,
-      newComments: post.comments,
-    });
-  }
+        result: true
+    })
+})
 
-  return res.json({
-    result: false,
-  });
-});
+
+
+postRoutes.delete('/posts', (req, res) => {
+
+    //delete login
+
+    return res.json({
+        result: true
+    })
+})
+
+
+
+
+postRoutes.put('/posts', (req, res) => {
+
+    return res.json({
+        result: true
+    })
+})
+
+
+
+//likes
+postRoutes.patch('/posts/:id/like', async (req, res) => {
+
+    let id = req.params.id;
+    let username = req.body.username;
+    //get item from database
+    let posts = await getAllItemsFromCollection('posts', { _id: ObjectId(id) })
+
+    //checking if found
+    if (posts && posts.length > 0) {
+
+        console.log('post preset')
+
+        let post = posts[0];
+        if (!post.likedUsers.some(x => x == username)) {
+
+            //update likes on server     
+            post.likedUsers.push(username)
+            post.likes = post.likedUsers.length;
+            
+            
+            //update likes on database
+            await updateItemFromCollection("posts", { _id: ObjectId(id) }, post)
+            //done
+            return res.json({
+                result: true,
+                newLikes: post.likes
+            })
+        } else {
+            return res.json({
+                result: true,
+                newLikes: post.likes
+            })
+        }
+
+    }
+
+    return res.json({
+        result: false
+    })
+})
+
+
+
+
+
+//likes
+postRoutes.post('/posts/:id/comments', (req, res) => {
+
+    let id = req.params.id;
+    let commentObj = req.body;
+    //get item from database
+    let postsList = JSON.parse(fs.readFileSync('./data/posts.json'))
+    let index = postsList.findIndex(x => x.id == id)
+
+    //checking if found
+    if (index > -1) {
+
+        let post = postsList[index];
+        post.comments.push(commentObj)
+
+        //save the file
+        fs.writeFileSync('./data/posts.json', JSON.stringify(postsList))
+        //done
+        return res.json({
+            result: true,
+            newComments: post.comments
+        })
+    }
+
+    return res.json({
+        result: false
+    })
+})
 
 export default postRoutes;
